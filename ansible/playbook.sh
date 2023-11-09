dab20fe719c479c8a6dff522796ecb250c98ea64
samboleap3008 <samboleap3008@gmail.com>
2023-11-10 03:57:58 +0700

    playbook

--- ansible/playbook.sh
+++ ansible/playbook.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ansible-playbook -i host.ini /roles/installNginx/playbook.yml &&
 ansible-playbook -i host.ini /roles/installJava/playbook.yml &&
 ansible-playbook -i host.ini /roles/installDocker/playbook.yml
