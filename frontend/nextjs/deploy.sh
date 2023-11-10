2841f1ba50262e06067ec4ae00a18823e23f9fe6
samboleap3008 <samboleap3008@gmail.com>
2023-11-10 09:47:51 +0700

    configure load balancing

---
+++ frontend/nextjs/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Jenkins Authentication 
+username="SBL"
+token="115cde8cb44bc1dbf8dff90a9985d34b76"
+jenkins_url="http://34.121.191.11:8080"
+
+#Create trigger build jobs for Jenkins
+read -p "Enter your Jenkins job name: " jenkins_job
+
+#
---
+++ nginx/loadBalancing.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Define the servers
+read -p "Enter the server1 IP address: " server1
+read -p "Server1 port: " port1
+read -p "Enter the server2 IP address: " server2
+read -p "Server2 port: " port2
+
+# Validate input values
+if [[ -z $server1 || -z $port1 || -z $server2 || -z $port2 ]]; then
+    echo "Server information cannot be empty. Exiting..."
+    exit 1
+fi
+
+# Prompt for domain and email information
+echo "Please enter your information needed to add a domain name certificate:"
+read -p "Enter the domain name of the server: " domain
+read -p "Enter your email address: " email
+
+# Validate domain and email input
+if [[ -z $domain || -z $email ]]; then
+    echo "Domain name and email address cannot be empty. Exiting..."
+    exit 1
+fi
+
+# Prompt for the Nginx configuration file name
+echo "Please enter the Nginx configuration file name:"
+read nginx_dns
+
+# Validate Nginx configuration file
+nginx_conf="/etc/nginx/sites-available/$nginx_dns"
+if [[ -e $nginx_conf ]]; then
+    echo "The specified Nginx configuration file already exists. Please choose a different file name. Exiting..."
+    exit 1
+fi
+
+# Install the required packages
+sudo apt update
+sudo apt install -y nginx certbot
+
+# Configure the Nginx server
+nginx_server="
+http {
+    upstream backend {
+        least_conn;
+        server $server1:$port1;
+        server $server2:$port2;
+        # Add more servers here
+    }
+
+    server {
+        listen 80;
+        server_name $domain;
+        return 301 https://\$host\$request_uri;
+    }
+
+    server {
+        listen 443 ssl http2;
+        server_name $domain;
+
+        ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+
+        location / {
+            proxy_pass http://backend;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        }
+    }
+}
+"
+
+echo "$nginx_server" | sudo tee "$nginx_conf" > /dev/null
+
+# Create a symbolic link between the sites-available and sites-enabled directories in Nginx
+sudo ln -s "$nginx_conf" /etc/nginx/sites-enabled/
+echo "Successfully linked the configuration file for '$nginx_dns' from sites-available to sites-enabled."
+
+# Test running with Nginx configuration
+sudo nginx -t
+echo "Nginx configuration test successful."
+
+# Obtain SSL certificate using Certbot
+sudo certbot --nginx --noninteractive --email "$email" -d "$domain"
+
+# Restart Nginx
+sudo systemctl restart nginx
